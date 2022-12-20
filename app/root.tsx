9d27c9e49da14b2dde5dc56e2998f913b2a2d371
Ryo Shen <shenlw66@gmail.com>
2022-12-20 23:05:48 +0800

    feat: add code lint

--- app/root.tsx
+++ app/root.tsx
@@ -1,16 +1,9 @@
-import type { MetaFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import type { MetaFunction } from '@remix-run/node';
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
 
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  viewport: "width=device-width,initial-scale=1",
+  charset: 'utf-8',
+  viewport: 'width=device-width,initial-scale=1'
 });
 
 export default function App() {
