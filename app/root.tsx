25941f62398f68fbd5ac2039bd5d13177d493c57
Ryo Shen <shenlw66@gmail.com>
2022-12-22 21:37:34 +0800

    feat: add global css

--- app/root.tsx
+++ app/root.tsx
@@ -1,10 +1,21 @@
-import type { MetaFunction } from '@remix-run/node';
+import type { MetaFunction, LinksFunction } from '@remix-run/node';
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+
+import globalStylesUrl from './styles/global.css';
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
   viewport: 'width=device-width,initial-scale=1'
 });
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: 'stylesheet',
+      href: globalStylesUrl
+    }
+  ];
+};
 
 export default function App() {
   return (
