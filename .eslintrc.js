e594fe653e8c30545dabeef5df65c92ebb36560d
Ryo Shen <shenlw66@gmail.com>
2022-12-22 21:36:57 +0800

    feat: add eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,25 @@
-/** @type {import('eslint').Linter.Config} */
 module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
   extends: [
     '@remix-run/eslint-config',
     '@remix-run/eslint-config/node',
     'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
     'plugin:prettier/recommended'
-  ]
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'warn'
+  }
 };
