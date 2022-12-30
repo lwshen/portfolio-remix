5d3dc2ef8cb13a3f2ca386bdc9b4e252f2d38e96
Ryo Shen <5581972+lwshen@users.noreply.github.com>
2022-12-30 22:37:09 +0800

    feat: add avatar (#10)
    * feat: add avatar
    
    * fix: lint css

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,8 +1,28 @@
+import type { LinksFunction } from '@remix-run/node';
+import * as Avatar from '@radix-ui/react-avatar';
+
 import { Name } from '~/components/Name';
+import AvatarImage from '~/assets/image/avatar.jpeg';
+import stylesUrl from '~/styles/components/avatar.css';
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: 'stylesheet',
+      href: stylesUrl
+    }
+  ];
+};
 
 export default function Index() {
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
+      <Avatar.Root className="AvatarRoot">
+        <Avatar.Image className="AvatarImage" src={AvatarImage} alt="Avatar Image" />
+        <Avatar.Fallback className="AvatarFallback" delayMs={600}>
+          Ryo
+        </Avatar.Fallback>
+      </Avatar.Root>
       <p>Slinvent</p>
       <p className="text-blue-700">Slinvent</p>
       <Name className="text-bold text-4xl" values={['aaa', 'bbb']} />
