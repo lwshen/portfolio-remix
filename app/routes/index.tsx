317ab21db1f203d6b68bd8152336f71a69b19110
Ryo Shen <5581972+lwshen@users.noreply.github.com>
2022-12-30 22:21:18 +0800

    feat: add name component (#9)

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,8 +1,11 @@
+import { Name } from '~/components/Name';
+
 export default function Index() {
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
       <p>Slinvent</p>
       <p className="text-blue-700">Slinvent</p>
+      <Name className="text-bold text-4xl" values={['aaa', 'bbb']} />
     </div>
   );
 }
