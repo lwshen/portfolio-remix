cc4ce8728f3e50366e5cb8067ddee607c0d9eb4d
Ryo Shen <shenlw66@gmail.com>
2023-01-02 20:22:05 +0800

    feat: add header and footer component

---
+++ app/components/HomeFooter.tsx
@@ -0,0 +1,7 @@
+export default function HomeFooter() {
+  return (
+    <div className="text-gray-400 p-4 text-center align-middle">
+      <span className="text-sm">Copyright © Slinvent</span>
+    </div>
+  );
+}
