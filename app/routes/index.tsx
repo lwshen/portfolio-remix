206388e25269e4379ca807263b710c5d3985bd6b
Ryo Shen <shenlw66@gmail.com>
2023-01-07 23:45:23 +0800

    feat: add Home component

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,5 @@
-import Name from '~/components/home/Name';
-import Avatar, { links as avatarLinks } from '~/components/home/Avatar';
-import AvatarImage from '~/assets/image/avatar.jpeg';
+import Home from '~/components/home/Home';
+import { links as avatarLinks } from '~/components/home/Avatar';
 
 export function links() {
   return [...avatarLinks()];
@@ -9,8 +8,7 @@ export function links() {
 export default function Index() {
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-      <Avatar className="w-16 h-16" imgUrl={AvatarImage} fallback="Ryo" />
-      <Name className="text-bold text-3xl" values={['aaa', 'bbb']} />
+      <Home />
     </div>
   );
 }
