2d1b507d8def9044db05b98bdda8d5c1b5344871
Ryo Shen <5581972+lwshen@users.noreply.github.com>
2023-03-19 03:37:27 +0000

    style: fix lint

--- remix.config.js
+++ remix.config.js
@@ -1,7 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ['**/.*'],
-  serverDependenciesToBundle: ['marked']
+  serverDependenciesToBundle: ['marked'],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
