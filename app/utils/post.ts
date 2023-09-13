98c90a95b0007c4e1cc31f3a096674fc75b9f3e5
Ryo Shen <shenlw66@gmail.com>
2023-09-13 18:15:16 +0800

    feat: highlight code in markdown

--- app/utils/post.ts
+++ app/utils/post.ts
@@ -1,10 +1,11 @@
 import fs from 'fs/promises';
 import matter from 'gray-matter';
-import { marked } from 'marked';
 import path from 'path';
 
 import type { DocAttribute, Post } from '~/types/post';
 import { DocAttributeSchema } from '~/types/post';
+
+import { renderMarkdown } from './markdown';
 
 export const validateAttribute = (fontMatter: Record<string, unknown>): DocAttribute =>
   DocAttributeSchema.parse(fontMatter);
@@ -35,22 +36,15 @@ export async function getPost(slug: string): Promise<Post> {
   const filepath = path.join(postsPath, slug + '.md');
   const { attribute: unsafeAttribute, content } = parseFrontMatter(filepath);
   const attribute = validateAttribute(unsafeAttribute);
-  return { slug, attribute, markdown: content, html: marked(content) };
+  return { slug, attribute, markdown: content, html: renderMarkdown(content) };
 }
 
 export async function getPosts(): Promise<Post[]> {
   const dir = await fs.readdir(postsPath);
   const posts = await Promise.all(
     dir.map(async filename => {
-      const filePath = path.join(postsPath, filename);
-      const { attribute: unsafeAttribute, content } = parseFrontMatter(filePath);
-      const attribute = validateAttribute(unsafeAttribute);
-      return {
-        slug: filename.replace(/\.md$/, ''),
-        attribute,
-        markdown: content,
-        html: marked(content),
-      };
+      const slug = filename.replace(/\.md$/, '');
+      return getPost(slug);
     })
   );
   return posts.sort((a, b) => b.attribute.date.getTime() - a.attribute.date.getTime());
