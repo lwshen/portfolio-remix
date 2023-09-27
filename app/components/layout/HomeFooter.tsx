132feffb3645932a1925c88df0df69909f0e0def
Ryo Shen <shenlw66@gmail.com>
2023-09-27 16:34:48 +0800

    refactor: Use useRootData hook in HomeFooter and HomeHeader components

--- app/components/layout/HomeFooter.tsx
+++ app/components/layout/HomeFooter.tsx
@@ -1,17 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useRootData } from '~/hooks/useRootData';
 
 export default function HomeFooter() {
-  const [beian, setBeian] = useState('');
-  useEffect(() => {
-    setBeian(window.ENV.BEIAN);
-  }, []);
+  const rootData = useRootData();
   const year = new Date().getFullYear();
   return (
     <div className="text-gray-400 px-4 py-16 text-center align-middle space-y-2">
       <div className="text-sm">Copyright 2022-{year} © Made With ❤ by Slinvent</div>
-      {beian.length > 0 && (
+      {rootData.env.BEIAN.length > 0 && (
         <a href="http://beian.miit.gov.cn" className="text-xs">
-          {beian}
+          {rootData.env.BEIAN}
         </a>
       )}
     </div>
